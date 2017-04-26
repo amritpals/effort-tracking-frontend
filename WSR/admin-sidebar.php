34fdb5fab64a908304cfbaa2d381ae253b8ebe1f
Amritpal <amritpals@users.noreply.github.com>
2017-04-26 17:46:00 +0530

    Update admin-sidebar.php

--- WSR/admin-sidebar.php
+++ WSR/admin-sidebar.php
@@ -7,24 +7,24 @@
   <div class="admin-sidebar-nav dropdown">
     <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-plus fa-lg" aria-hidden="true"></i>Create</a>
     <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-      <li><a href="admin-create-user.php">User</a></li>
+      <li><a href="admin-create-user.php">Resource</a></li>
       <li><a href="admin-create-project.php">Project</a></li>
-      <li><a href="admin-create-category.php">Category</a></li>
+      <li><a href="admin-create-category.php">Task Category</a></li>
     </ul>
   </div>
   <div class="admin-sidebar-nav dropdown">
-    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-check-square-o fa-lg" aria-hidden="true"></i>Assign</a>
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-check-square-o fa-lg" aria-hidden="true"></i>Allocate</a>
     <ul class="dropdown-menu">
-      <li><a href="admin-assign-user.php">Users</a></li>
-      <li><a href="admin-assign-category.php">Category</a></li>
+      <li><a href="admin-assign-user.php">Resource</a></li>
+      <li><a href="admin-assign-category.php">Task Category</a></li>
     </ul>
   </div>
   <div class="admin-sidebar-nav dropdown">
     <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-eye fa-lg" aria-hidden="true"></i>View</a>
     <ul class="dropdown-menu">
-      <li><a href="#">Users</a></li>
-      <li><a href="#">Projects</a></li>
-      <li><a href="#">Category</a></li>
+      <li><a href="admin-view-user.php">Resource</a></li>
+      <li><a href="admin-view-project.php">Project</a></li>
+      <li><a href="admin-view-category.php">Task Category</a></li>
     </ul>
   </div>
   <div class="admin-sidebar-nav">
