41ce0c855eaa01c3fbf7d16bbc2cf09032513a2a
Amritpal <amritpals@users.noreply.github.com>
2017-04-26 17:39:58 +0530

    Update admin-assign-user.php

--- WSR/admin-assign-user.php
+++ WSR/admin-assign-user.php
@@ -2,8 +2,9 @@
 <?php include 'admin-sidebar.php'; ?>
 
 		<div class="container app-container">
+
 			<div class="page-title">
-				<h2>Assign User</h2>
+				<h2>Allocate Resource</h2>
 			</div>
 			<div class="app-section assign-user-form">
 				<form id="assign-user">
@@ -20,6 +21,26 @@
 					<button type="submit" class="btn btn-primary">Save</button>
 				</form>
 			</div><!-- End of assign user form -->
+
+			<div class="page-title">
+				<h2>Remove Resource</h2>
+			</div>
+			<div class="app-section remove-user-form">
+				<form id="remove-user">
+					<div class="form-group">
+						<label for="projectRemove">Select Project</label>
+						<select class="form-control" name="projectRemove" id="projectRemove">
+						</select>
+					</div>
+					<div class="form-group">
+						<label for="userRemove">Select User</label>
+						<select class="form-control" name="userRemove" id="userRemove" multiple="multiple">
+						</select>
+					</div>
+					<button type="submit" class="btn btn-primary">Save</button>
+				</form>
+			</div><!-- End of remove user form -->
+
 		</div><!-- End of app-container -->
 
 <?php include 'footer.php'; ?>
