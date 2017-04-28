121e815e02986aa55ea2e71b8a119c12ee4b3082
Amritpal <amritpals@users.noreply.github.com>
2017-04-28 16:21:56 +0530

    Add files via upload

---
+++ WSR/user-home-screen.php
@@ -0,0 +1,32 @@
+<?php include 'header.php'; ?>
+<?php include 'user-sidebar.php'; ?>
+
+		<div class="container app-container">
+			<div class="page-title add-shadow">
+				<h2>Profile</h2>
+				<button type="submit" class="btn btn-primary logout-btn pull-right">Log Out</button>
+			</div>
+			<div class="app-section user-projects">
+				<h2>My Projects</h2>
+				<div class="project-item add-shadow">
+					<i class="fa fa-file fa-lg" aria-hidden="true"></i>
+					<h3>Project Name</h3>
+					<div class="pull-right">
+						<button type="button" class="btn btn-primary"><i class="fa fa-eye" aria-hidden="true"></i> View Report</button>
+						<button type="button" class="btn btn-primary"><i class="fa fa-clock-o" aria-hidden="true"></i> Log Effort </button>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="project-item add-shadow">
+					<i class="fa fa-file fa-lg" aria-hidden="true"></i>
+					<h3>Project Name</h3>
+					<div class="pull-right">
+						<button type="button" class="btn btn-primary"><i class="fa fa-eye" aria-hidden="true"></i> View Report</button>
+						<button type="button" class="btn btn-primary"><i class="fa fa-clock-o" aria-hidden="true"></i> Log Effort </button>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div><!-- End of assign user form -->
+		</div><!-- End of app-container -->
+
+<?php include 'footer.php'; ?>
---
+++ WSR/user-sidebar.php
@@ -0,0 +1,24 @@
+<aside class="admin-panel">
+  <div class="branding admin-branding">
+    <img src="images/report.png"/>
+    <h1>WSR</h1>
+    <!--p>Admin</p-->
+  </div>
+  <div class="user-details-sidebar">
+    <img id="user-profile-icon" src="images/profile.svg" width="150" height="150"/>
+    <table>
+      <tr>
+        <td class="text-center"><i class="fa  fa-user" aria-hidden="true"></i></td>
+        <td><p>Amritpal Singh</p></td>
+      </tr>
+      <tr>
+        <td class="text-center"><i class="fa  fa-star" aria-hidden="true"></i></td>
+        <td><p>Technical Lead</p></td>
+      </tr>
+      <tr>
+        <td class="text-center"><i class="fa  fa-address-card" aria-hidden="true"></i></td>
+        <td><p>Admin</p></td>
+      </tr>
+    </table>
+  </div>
+</aside>
