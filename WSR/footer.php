1ca75f38e260d061adf0bdb872ca57f024f2b62f
Amritpal <amritpals@users.noreply.github.com>
2017-05-02 16:59:26 +0530

    Update footer.php

--- WSR/footer.php
+++ WSR/footer.php
@@ -1,11 +1,12 @@
 
-  <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-  <script src="js/jquery.bootstrap-dropdown-hover.js"></script>
+  <script src="js/jquery-3.2.1.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.bootstrap-dropdown-hover.min.js"></script>
   <script src="js/bootstrap-multiselect.js"></script>
   <script src="js/restapi.js"></script>
   <script src="js/restoperation.js"></script>
   <script src="js/assign.js"></script>
+  <script src="js/view.js"></script>
   <script type="text/javascript">
     $(document).ready(function() {
 
