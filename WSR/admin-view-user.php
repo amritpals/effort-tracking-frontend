426bad2b6c720d0b6003aa3836d06e40c3c002cd
Amritpal <amritpals@users.noreply.github.com>
2017-05-02 16:56:23 +0530

    Update admin-view-user.php

--- WSR/admin-view-user.php
+++ WSR/admin-view-user.php
@@ -7,14 +7,11 @@
 				<button type="submit" class="btn btn-primary logout-btn pull-right">Log Out</button>
 			</div>
 			<div class="app-section create-user-form">
-				<form>
+				<form id="admin-view-user">
 					<div class="form-group">
 						<label for="userSelect">Select User</label>
 						<select class="form-control" id="userSelect">
-						  <option value="Abcd">Abcd</option>
-						  <option value="Defh">Defh</option>
-						  <option value="Ghij">Ghij</option>
-						  <option value="Jklm">Jklm</option>
+							<<option value="" selected disabled>Select the Resource</option>
 						</select>
 					</div>
 
@@ -22,34 +19,64 @@
 						<div class="col-md-12">
 							<legend>User Details:</legend>
 						</div>
-						<div class="col-md-6">
+
+						<div class="col-md-6"> <!-- Start of first column -->
+
 							<div class="form-group">
-								<label for="firstNameInput">First Name</label>
-								<input type="text" class="form-control" id="firstNameInput">
+								<label for="firstName">First Name</label>
+								<input type="text" class="form-control" id="firstName" name="firstName">
 							</div>
+
 							<div class="form-group">
-								<label for="lastNameInput">Last Name</label>
-								<input type="text" class="form-control" id="lastNameInput">
+								<label for="lastName">Last Name</label>
+								<input type="text" class="form-control" id="lastName" name="lastName">
 							</div>
+
 							<div class="form-group">
-								<label for="roleSelect">Role</label>
-								<select class="form-control" id="roleSelect">
-								  <option>Team Leader</option>
-								  <option>Engineer</option>
+								<label for="emailId">Email Address</label>
+								<input type="email" class="form-control" id="emailId" name="emailId">
+							</div>
+
+							<div class="form-group">
+								<label for="role">Role</label>
+								<select class="form-control" id="role" name="role">
+									<option value="Manager">Manager</option>
+								  <option value="Technical Lead">Technical Lead</option>
+								  <option value="Engineer">Engineer/Tester</option>
 								</select>
 							</div>
-							<!----- Radio Buttons -->
+
+							<div class="form-group">
+								<label for="dob">Date of Birth</label>
+								<input type="date" class="form-control" id="dob" name="dob" placeholder="YYYY-MM-DD">
+							</div>
+
+						</div> <!-- End of first column -->
+
+						<div class="col-md-6"> <!-- Start of second column -->
+
+							<div class="form-group">
+								<label for="wwid">WWID</label>
+								<input type="text" class="form-control" id="wwid" name="wwid">
+							</div>
+
+							<div class="form-group">
+								<label for="wiproEmpId">Wipro Employee ID</label>
+								<input type="text" class="form-control" id="wiproEmpId" name="wiproEmpId">
+							</div>
+
+							<!-- Radio Buttons -->
 							<div class="row form-group">
 								<label class="col-sm-2">Admin?</label>
 								<div class="col-sm-10">
 									<div class="form-check form-check-inline col-md-3">
 									  <label class="form-check-label">
-										<input class="form-check-input" type="radio" name="adminRadioOptions" id="adminRadio1" value="option1"> Yes
+										<input class="form-check-input" type="radio" name="isAdmin" id="adminRadio1" value="true"> Yes
 									  </label>
 									</div>
 									<div class="form-check form-check-inline col-md-3">
 									  <label class="form-check-label">
-										<input class="form-check-input" type="radio" name="adminRadioOptions" id="adminRadio2" value="option2"> No
+										<input class="form-check-input" type="radio" name="isAdmin" id="adminRadio2" value="false"> No
 									  </label>
 									</div>
 								</div>
@@ -59,24 +86,45 @@
 								<div class="col-sm-10">
 									<div class="form-check form-check-inline col-md-3">
 									  <label class="form-check-label">
-										<input class="form-check-input" type="radio" name="mgrRadioOptions" id="mgrRadio1" value="option1"> Yes
+										<input class="form-check-input" type="radio" name="isManager" id="mgrRadio1" value="true"> Yes
 									  </label>
 									</div>
 									<div class="form-check form-check-inline col-md-3">
 									  <label class="form-check-label">
-										<input class="form-check-input" type="radio" name="mgrRadioOptions" id="mgrRadio2" value="option2"> No
+										<input class="form-check-input" type="radio" name="isManager" id="mgrRadio2" value="false"> No
 									  </label>
 									</div>
 								</div>
 							</div>
 
-						</div>
-						<div class="col-md-6">
-							<div class="form-group">
-								<label for="idInput">ID</label>
-								<input type="text" class="form-control" id="idInput">
+							<div class="row form-group">
+								<label class="col-sm-2">Enabled?</label>
+								<div class="col-sm-10">
+									<div class="form-check form-check-inline col-md-3">
+									  <label class="form-check-label">
+										<input class="form-check-input" type="radio" name="isEnabled" id="enabledRadio1" value="true"> Yes
+									  </label>
+									</div>
+									<div class="form-check form-check-inline col-md-3">
+									  <label class="form-check-label">
+										<input class="form-check-input" type="radio" name="isEnabled" id="enabledRadio2" value="false"> No
+									  </label>
+									</div>
+								</div>
 							</div>
-						</div>
+
+							<div class="form-group">
+								<label for="projectName">Allocated Project(s)</label>
+								<input type="text" class="form-control" id="projectName" name="projectName" readonly="readonly">
+								<input type="hidden" class="form-control" id="id" name="id">
+								<input type="hidden" class="form-control" id="billingModel" name="billingModel">
+								<input type="hidden" class="form-control" id="budget" name="budget">
+								<input type="hidden" class="form-control" id="isEnabled" name="isEnabledProject">
+								<input type="hidden" class="form-control" id="intelManagerName" name="intelManagerName">
+							</div>
+
+						</div> <!-- End of second column -->
+
 					</div><!-- End of row 1 -->
 					<button type="submit" class="btn btn-primary">Submit</button>
 				</form>
