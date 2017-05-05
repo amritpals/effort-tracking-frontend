bebf00d5b3f5bd40de09b1d6a2aef73c5af72c6a
Amritpal <amritpals@users.noreply.github.com>
2017-05-05 14:03:46 +0530

    Update header.php

--- WSR/header.php
+++ WSR/header.php
@@ -6,8 +6,9 @@
 		<title>Log In - WSR</title>
 		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
 		<link rel="stylesheet" type="text/css" href="css/normalize.css">
-		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css" rel="stylesheet" />
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css.map">
+		<link href="css/font-awesome.min.css" type="text/css" rel="stylesheet" />
 		<link rel="stylesheet" type="text/css" href="css/bootstrap-multiselect.css">
 		<link href="css/style.css" type="text/css" rel="stylesheet" />
 	</head>
